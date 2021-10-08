607d303bf377a74be48afa84402c2f8c9f5e0f78
Willem de Groot <gwillem@gmail.com>
2021-10-08 10:07:39 +0200

    Fix SafeNil references in stream.go

--- stream.go
+++ stream.go
@@ -179,10 +179,9 @@ func nonSpace(b []byte) bool {
 
 // An Encoder writes JSON values to an output stream.
 type Encoder struct {
-	w                 io.Writer
-	err               error
-	escapeHTML        bool
-	nilSafeCollection bool
+	w          io.Writer
+	err        error
+	escapeHTML bool
 
 	indentBuf    *bytes.Buffer
 	indentPrefix string
@@ -204,7 +203,7 @@ func (enc *Encoder) Encode(v interface{}) error {
 		return enc.err
 	}
 	e := newEncodeState()
-	err := e.marshal(v, encOpts{escapeHTML: enc.escapeHTML, nilSafeCollection: enc.nilSafeCollection})
+	err := e.marshal(v, encOpts{escapeHTML: enc.escapeHTML})
 	if err != nil {
 		return err
 	}
@@ -253,12 +252,6 @@ func (enc *Encoder) SetIndent(prefix, indent string) {
 // of the output, SetEscapeHTML(false) disables this behavior.
 func (enc *Encoder) SetEscapeHTML(on bool) {
 	enc.escapeHTML = on
-}
-
-// SetNilSafeCollection specifies whether to represent nil slices and maps as
-// '[]' or '{}' respectfully (flag on) instead of 'null' (default) when marshaling json.
-func (enc *Encoder) SetNilSafeCollection(on bool) {
-	enc.nilSafeCollection = on
 }
 
 // RawMessage is a raw encoded JSON value.
